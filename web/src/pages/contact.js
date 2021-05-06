76e1a937e7c19861ff353339c9f710a988b449be
Sheri Richardson <shrdson@gmail.com>
2021-05-06 22:35:16 +0200

    Add pages

---
+++ web/src/pages/contact.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const ContactPage = () => {
+  return (
+    <main>
+      <h1>Contact - Designo</h1>
+    </main>
+  );
+};
+
+export default ContactPage;
---
+++ web/src/pages/locations.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const LocationsPage = () => {
+  return (
+    <main>
+      <h1>Locations - Designo</h1>
+    </main>
+  );
+};
+
+export default LocationsPage;
