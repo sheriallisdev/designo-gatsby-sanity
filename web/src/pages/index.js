51a35467675b50ea72f858a199698efc9606d5ec
Sheri Richardson <shrdson@gmail.com>
2021-05-14 22:16:05 +0200

    Add workflow grid

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,12 +1,14 @@
 import * as React from "react";
 import HomepageHero from "../components/HomepageHero";
 import ProjectLinkGrid from "../components/ProjectLinkGrid";
+import WorkflowGrid from "../components/WorkflowGrid";
 
 const IndexPage = () => {
   return (
     <main>
       <HomepageHero />
       <ProjectLinkGrid />
+      <WorkflowGrid />
     </main>
   );
 };
