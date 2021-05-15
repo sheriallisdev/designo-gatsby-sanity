a23931fbaa01581b0b6aee789efa2daffad2a85c
Sheri Richardson <shrdson@gmail.com>
2021-05-15 19:28:58 +0200

    Add footer

--- web/src/styles/GlobalStyles.js
+++ web/src/styles/GlobalStyles.js
@@ -7,7 +7,7 @@ const GlobalStyle = createGlobalStyle`
     --peach: hsl(11, 73%, 66%);
     --light-peach: hsl(11, 100%, 80%);
     --dark-grey: hsl(264, 5%, 20%);
-    --light-grey: hsl(210, 17, 95);
+    --light-grey: hsl(210, 17%, 95%);
     --site-container: 1111px;
   }
   html {
