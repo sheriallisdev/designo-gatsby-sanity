35aa5290edd47e8e87930c0ae6da941c0e634f93
Sheri Richardson <shrdson@gmail.com>
2021-05-17 21:50:18 +0200

    Remove main element from Layout

--- web/src/components/Layout.js
+++ web/src/components/Layout.js
@@ -11,7 +11,7 @@ export default function Layout({ children }) {
       <Typography />
       <GlobalStyles />
       <Nav />
-      <main>{children}</main>
+      {children}
       <Footer />
     </>
   );
