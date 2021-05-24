99988581ea6f15cc32ec980dce06867d271572fd
Sheri Richardson <shrdson@gmail.com>
2021-05-24 13:45:09 +0200

    Add location links

--- web/src/pages/contact.js
+++ web/src/pages/contact.js
@@ -1,9 +1,11 @@
 import * as React from "react";
+import LocationLinks from "../components/LocationLinks";
 
 const ContactPage = () => {
   return (
     <main>
       <h1>Contact - Designo</h1>
+      <LocationLinks />
     </main>
   );
 };
