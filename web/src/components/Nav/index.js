710318e97c1954c9eb12fb83f146120973d65e55
Sheri Richardson <shrdson@gmail.com>
2021-06-02 15:51:19 +0200

    Refactor mobile menu toggle

--- web/src/components/Nav/index.js
+++ web/src/components/Nav/index.js
@@ -8,11 +8,7 @@ import iconHamburger from "../../images/icon-hamburger.svg";
 const Nav = () => {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
 
-  function handleCloseMenu() {
-    setMenuIsOpen(false);
-  }
-
-  function handleToggleMenu() {
+  function handleToggle() {
     setMenuIsOpen(!menuIsOpen);
   }
 
@@ -23,22 +19,22 @@ const Nav = () => {
       </Link>
       <ul className={menuIsOpen ? "active" : ""}>
         <li>
-          <Link to="/about" onClick={handleCloseMenu}>
+          <Link to="/about" onClick={handleToggle}>
             Our Company
           </Link>
         </li>
         <li>
-          <Link to="/locations" onClick={handleCloseMenu}>
+          <Link to="/locations" onClick={handleToggle}>
             Locations
           </Link>
         </li>
         <li>
-          <Link to="/contact" onClick={handleCloseMenu}>
+          <Link to="/contact" onClick={handleToggle}>
             Contact
           </Link>
         </li>
       </ul>
-      <button className="toggle" onClick={handleToggleMenu}>
+      <button className="toggle" onClick={handleToggle}>
         <img src={menuIsOpen ? iconClose : iconHamburger} alt="open menu" />
       </button>
       <div className={menuIsOpen ? "overlay-active" : "overlay"}></div>
