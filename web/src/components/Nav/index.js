21388895d66549a81957b07deb1941aba9deee43
Sheri Richardson <shrdson@gmail.com>
2021-06-02 16:20:24 +0200

    Refactor mobile menu toggle (#10)
    * Refactor mobile menu toggle
    
    * Close menu when navigating to home route

--- web/src/components/Nav/index.js
+++ web/src/components/Nav/index.js
@@ -8,37 +8,33 @@ import iconHamburger from "../../images/icon-hamburger.svg";
 const Nav = () => {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
 
-  function handleCloseMenu() {
-    setMenuIsOpen(false);
-  }
-
-  function handleToggleMenu() {
+  function handleToggle() {
     setMenuIsOpen(!menuIsOpen);
   }
 
   return (
     <Styled.Nav>
-      <Link to="/" className="logoLink">
+      <Link to="/" className="logoLink" onClick={() => setMenuIsOpen(false)}>
         <Logo variant="dark" alt="Designo logo" />
       </Link>
       <ul className={menuIsOpen ? "active" : ""}>
         <li>
-          <Link to="/about" onClick={handleCloseMenu}>
+          <Link to="/about" onClick={handleToggle}>
             Our Company
           </Link>
         </li>
         <li>
-          <Link to="/locations" onClick={handleCloseMenu}>
+          <Link to="/locations" onClick={handleToggle}>
             Locations
           </Link>
         </li>
         <li>
-          <Link to="/contact" onClick={handleCloseMenu}>
+          <Link to="/contact" onClick={handleToggle}>
             Contact
           </Link>
         </li>
       </ul>
-      <button className="toggle" onClick={handleToggleMenu}>
+      <button className="toggle" onClick={handleToggle}>
         <img src={menuIsOpen ? iconClose : iconHamburger} alt="open menu" />
       </button>
       <div className={menuIsOpen ? "overlay-active" : "overlay"}></div>
