6d87e4b321c9395b8f8500ac261399a379c78fa9
Sheri Richardson <shrdson@gmail.com>
2021-06-14 18:38:08 +0200

    wip

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -33,3 +33,18 @@ exports.createPages = async ({ graphql, actions }) => {
     });
   });
 };
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /offending-module/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    });
+  }
+};
